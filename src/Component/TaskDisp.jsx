34a160339fba143f25427ef80d722b4ce2bc479c
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-07-19 19:51:49 +0530

    done

--- src/Component/TaskDisp.jsx
+++ src/Component/TaskDisp.jsx
@@ -2,15 +2,20 @@ import React, { useContext } from 'react';
 import { Context } from '../Context/Context';
 // import { MdDelete } from "react-icons/md";
 import { MdDelete, MdEdit, MdSave } from "react-icons/md";
+import { useState } from 'react';
 
 const TaskDisp = () => {
     const { Task,setTask } = useContext(Context);
+    const [isEditing, setIsEditing] = useState(null);
+
 const handleRemoveTask=(id)=>{
     setTask(Task.filter(Task=>Task.id!==id))
 
 }
 const handleEditTask=(id,task)=>{
-    
+    setIsEditing(id)
+    setTask(task)
+
 
 }
     return (
@@ -18,9 +23,31 @@ const handleEditTask=(id,task)=>{
             <div className='task-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
                 {Task?.map((item, index) => (
                     <div key={index} className='shadow-lg border bg-white rounded-lg p-5 m-2 hover:shadow-xl transition-shadow duration-300  flex justify-between'>
-                        <p className='text-lg font-semibold text-gray-800'>{item.text}</p>
-                        <button className='text-[25px] text-blue-500'onClick={()=>handleEditTask(item.id,item.text)}><MdEdit/></button>
-                        <button type='button' className='text-[25px] hover:text-red-700 duration-150' onClick={()=>handleRemoveTask(item.id)}><MdDelete/></button>
+                        {
+                            isEditing==item.id?
+                            (
+                                <div className='flex flex-col'>
+                                    <input
+                                    type='text'
+                                    value={Task}
+                                    onChange={(e) => setTask(e.target.value)}
+                                    className='p-2 border rounded mb-2'
+                                />
+
+
+                                </div>
+                            ):
+                            (
+<>
+<p className='text-lg font-semibold text-gray-800'>{item.text}</p>
+                            <button className='text-[25px] text-blue-500'onClick={()=>handleEditTask(item.id,item.text)}><MdEdit/></button>
+                            <button type='button' className='text-[25px] hover:text-red-700 duration-150' onClick={()=>handleRemoveTask(item.id)}><MdDelete/></button>
+</>
+                            )
+                            
+                        }
+                        
+
                     </div>
                 ))}
             </div>
