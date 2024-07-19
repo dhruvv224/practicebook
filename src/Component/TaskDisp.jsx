59865c41f4b5d4adeecd2be2840108a40c9b9d44
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-07-19 19:31:15 +0530

    done

--- src/Component/TaskDisp.jsx
+++ src/Component/TaskDisp.jsx
@@ -1,11 +1,16 @@
 import React, { useContext } from 'react';
 import { Context } from '../Context/Context';
-import { MdDelete } from "react-icons/md";
+// import { MdDelete } from "react-icons/md";
+import { MdDelete, MdEdit, MdSave } from "react-icons/md";
 
 const TaskDisp = () => {
     const { Task,setTask } = useContext(Context);
 const handleRemoveTask=(id)=>{
     setTask(Task.filter(Task=>Task.id!==id))
+
+}
+const handleEditTask=(id,task)=>{
+    
 
 }
     return (
@@ -14,6 +19,7 @@ const handleRemoveTask=(id)=>{
                 {Task?.map((item, index) => (
                     <div key={index} className='shadow-lg border bg-white rounded-lg p-5 m-2 hover:shadow-xl transition-shadow duration-300  flex justify-between'>
                         <p className='text-lg font-semibold text-gray-800'>{item.text}</p>
+                        <button className='text-[25px] text-blue-500'onClick={()=>handleEditTask(item.id,item.text)}><MdEdit/></button>
                         <button type='button' className='text-[25px] hover:text-red-700 duration-150' onClick={()=>handleRemoveTask(item.id)}><MdDelete/></button>
                     </div>
                 ))}
